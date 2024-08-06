9727b14d35205e313c75a9864a543186f3f75ea5
Avestura <me@avestura.dev>
2024-08-06 08:30:28 +0330

    feat: blink block vs line

--- profile.ps1
+++ profile.ps1
@@ -5,9 +5,15 @@ Set-PSReadLineOption -PredictionViewStyle ListView -PredictionSource HistoryAndP
     if ($mode -eq "Command") {
         $Env:VI_MODE_SEGMENT = "COM"
         [System.Console]::CursorSize = 100
+        
+        # Set the cursor to a blinking block.
+        Write-Host -NoNewLine "`e[1 q"
     } else {
         $Env:VI_MODE_SEGMENT = "INS"
         [System.Console]::CursorSize = 30
+
+        # Set the cursor to a blinking line.
+        Write-Host -NoNewLine "`e[5 q"
     }
 
     [Microsoft.PowerShell.PSConsoleReadLine]::InvokePrompt()
